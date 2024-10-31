6186a926f4901a01640578ee0ea7772525ac3c99
James Hart <jeh27@st-andrews.ac.uk>
2024-10-31 18:47:27 +0000

    Movable multiple circles with input

--- fsa-visualiser/src/Viewport.js
+++ fsa-visualiser/src/Viewport.js
@@ -1,6 +1,6 @@
 import styles from "./styles/Viewport.module.css"
 import useMouse from "@react-hook/mouse-position" // https://www.npmjs.com/package/@react-hook/mouse-position
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { addState } from "./FSA"
 import Draggable from 'react-draggable'; // https://www.npmjs.com/package/react-draggable
 
@@ -12,6 +12,8 @@ const circleRadius = 50;
 export const Viewport = ({ machine, setMachine }) => {
   const [clickLock, setClickLock] = useState(true);
   const [circleArray, setCircleArray] = useState([]);
+  const [coordArray, setCoordArray] = useState([]);
+
   // Mouse tracking copied from: https://www.npmjs.com/package/@react-hook/mouse-position
   const ref = useRef(null)
   const mouse = useMouse(ref, {})
@@ -20,25 +22,42 @@ export const Viewport = ({ machine, setMachine }) => {
   // User Click on Viewport
   if (mouse.isDown && clickLock) {
     setClickLock(false);
+    // Make a check that if the mouse coordinates are on a circle already in array, do not proceed
 
-    setCircleArray(
-      [...circleArray,
-      <Draggable defaultPosition={{ x: mouse.x - circleRadius, y: mouse.y - circleRadius }}>
-        <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-          <circle className="circle" r={circleRadius - 5} cx={circleRadius} cy={circleRadius} fill="white" stroke="black" stroke-width="2"/>
-        </svg>
-      </Draggable>
-      ])
-    setMachine(addState(machine, "Unnamed"));
+    // Change so array is of CURRENT CORRDINATES
+    let flag = false;
+    coordArray.forEach(element => {
+      if ((mouse.x > element[0] - 50) && (mouse.x < element[0] + 50)
+        && (mouse.y > element[1] - 50) && (mouse.y < element[1] + 50)) {
+        flag = true;
+      }
+    });
+
+    if (!flag) {
+      setCircleArray(
+        [...circleArray,
+        <Draggable key={circleArray.length} defaultPosition={{ x: mouse.x - circleRadius, y: mouse.y - circleRadius }}>
+          <div>
+            <input className={styles.stateInput} type="text" defaultValue={"Unnamed"} onChange={(e) => setMachine(addState(machine, e.target.value))}
+              style={{ height: circleRadius * 1.5, width: circleRadius * 1.5, textAlign: "center" }} />
+          </div>
+        </Draggable>
+        ])
+
+      setCoordArray(
+        [...coordArray,
+        [mouse.x, mouse.y]
+        ]
+      )
+    }
+
+    setClickLock(!clickLock);
   }
 
-  // if (!mouse.isDown) {
-  //   setClickLock(true);
-  // }
-
-  // if (!mouse.isDown) {
-  //   setClickLock(true);
-  // }
+  // Locks clicking so one click = one state made
+  useEffect(() => {
+    setClickLock(!mouse.isDown)
+  }, [clickLock, setClickLock, mouse.isDown]);
 
   return <div className={styles.Viewport} ref={ref}>
     {circleArray}
