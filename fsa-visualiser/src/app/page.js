c56794f55e2492338b3261ad7b89586947b1d594
James Hart <jeh27@st-andrews.ac.uk>
2025-03-30 14:12:16 +0100

    Project Organising

--- fsa-visualiser/src/app/page.js
+++ fsa-visualiser/src/app/page.js
@@ -6,13 +6,16 @@ import { useState } from "react";
 import { InteractionWindow } from './components/InteractionWindow';
 
 function App() {
-  const [machine, setMachine] = useState(new FSA(0));
-  const [organiseLayout, setOrganiseLayout] = useState(false);
   // Creates a state for the machine, which may be updated during runtime 
+  const [machine, setMachine] = useState(new FSA(0));
+
+  // Tracks whether the 'Organise FSA Layout' button has been pressed.
+  const [organiseLayout, setOrganiseLayout] = useState(false);
 
   return ( // The JSX for the application
     <div data-testid="App" style={{ height: "100%", width: "100%" }}
       onContextMenu={(event) => event.preventDefault()}> {/*  Disables default right-click behaviour (browser drop downs) */}
+
       <Viewport machine={machine} setMachine={setMachine} organiseLayout={organiseLayout} setOrganiseLayout={setOrganiseLayout} />
       <InteractionWindow machine={machine} setMachine={setMachine} setOrganiseLayout={setOrganiseLayout} />
     </div>
